e54664633f588a4da0ba7171e15c826be8f90995
Procy0n <127360356+Procy0n450@users.noreply.github.com>
2024-02-11 18:16:41 +0100

    Update and rename Cache to Cache.lua

--- Cache.lua
+++ Cache.lua
@@ -11,6 +11,7 @@ AddEventHandler('esx:MiseEnCache', function()
 		CachePlayer[xPlayer.source].job = xPlayer.job.name 
 		CachePlayer[xPlayer.source].job2 = xPlayer.job2.name 
 		CachePlayer[xPlayer.source].name = GetPlayerName(xPlayer.source)
+		-- Ajouter ce que vous voulez mettre en cache 
 		print("[^4"..CachePlayer[xPlayer.source].players.."^7] (^2Mise en cache^7) Joueur : (^4"..GetPlayerName(CachePlayer[xPlayer.source].players).."^7) Groupe : (^4"..CachePlayer[xPlayer.source].group.."^7) Jobs (^4"..CachePlayer[xPlayer.source].job.."^7) Crew : (^4"..CachePlayer[xPlayer.source].job2.."^7)")
 	end
 end)
@@ -23,4 +24,15 @@ end)
 
 function ESX.GetCachingPlayer()
 	return CachePlayer
-end
+end 
+
+-- Exemple server 
+RegisterNetEvent('alerte:police')
+AddEventHandler('alerte:police', function()
+     local source = source 
+     for k,v in pairs(ESX.GetCachingPlayer()) do 
+	if v.job == "police" then 
+	    TriggerClientEvent('esx:showAdvancedNotification', v.players, 'Alerte Police', 'Alerte Police', "Braquage en cours", 'CHAR_EXEMPLE', 1)
+	end
+      end 
+end)
